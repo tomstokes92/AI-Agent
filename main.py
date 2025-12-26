1882bd1f48f74874277a381c1280ae1290fe27d7
tomstokes92 <stokes.thomas@gmail.com>
2025-12-26 15:50:04 +0000

    Adding Calculator Function

--- main.py
+++ main.py
@@ -1,8 +1,41 @@
 import os
+import argparse
 from dotenv import load_dotenv
+from google import genai
+from google.genai import types
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-print(api_key)
-if api_key == None:
-    raise RuntimeError("Invalid API Key")
+
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    # Check for presence of API Key
+    if api_key == None:
+        raise RuntimeError("Invalid API Key")
+    
+    client = genai.Client(api_key=api_key)
+
+    parser = argparse.ArgumentParser(description="Tom's Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
+
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
+    # API call and response using args.user_prompt as input
+    response = client.models.generate_content(model='gemini-2.5-flash', contents=messages)
+    if response.usage_metadata == None:
+        raise RuntimeError("Response Metadata empty, check API request")
+    
+    user_prompt = args.user_prompt
+    prompt_tokens = response.usage_metadata.prompt_token_count
+    response_tokens = response.usage_metadata.candidates_token_count
+
+    if args.verbose:
+        print(f"User prompt: {user_prompt}\nPrompt tokens: {prompt_tokens}\nResponse tokens: {response_tokens}")
+        print(f"Response: \n{response.text}")
+    else:
+        print(f"Response: \n{response.text}")
+
+
+if __name__ == "__main__":
+    main()
