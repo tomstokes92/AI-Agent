6e37cc98deebf7c7101137f028c03c30d1122221
tomstokes92 <stokes.thomas@gmail.com>
2025-12-26 17:50:58 +0000

    Adding and debugging get_files_info

---
+++ test_get_files_info.py
@@ -0,0 +1,23 @@
+from functions.get_files_info import get_files_info
+
+
+def main():
+    print("Result for current directory:")
+    print(get_files_info("calculator", "."))
+    print()
+
+    print("Result for 'pkg' directory:")
+    print(get_files_info("calculator", "pkg"))
+    print()
+
+    print("Result for '/bin' directory:")
+    print(get_files_info("calculator", "/bin"))
+    print()
+
+    print("Result for '../' directory:")
+    print(get_files_info("calculator", "../"))
+    print()
+
+
+if __name__ == "__main__":
+    main()
